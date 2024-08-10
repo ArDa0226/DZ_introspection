e3189dbd2d6438da5f90a06fd05f96f82957bc3c
ArDa0226 <161866557+ArDa0226@users.noreply.github.com>
2024-08-10 13:51:54 +0500

    DZ_introspection

---
+++ DZ_introspection.py
@@ -0,0 +1,27 @@
+
+def introspection_info(obj):
+
+    object_type = type(obj)
+
+    attribute = dir(obj)
+
+    methods = [method for method in attribute if callable(getattr(obj, method))]
+
+    module = obj.__class__.__module__
+
+    information = {'type': object_type,
+                   'attribute': attribute,
+                   'methods': methods,
+                   'module': module}
+
+    return information
+
+number_information = introspection_info(23)
+print(number_information)
+
+str_information = introspection_info('Привет')
+print(str_information)
+
+list_information = introspection_info([2, 34, 4.5, 'Hello'])
+print(list_information)
+

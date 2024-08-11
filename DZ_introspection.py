64b2e4f05b8e55a4024a9afc63351c4d6a2a48f5
ArDa0226 <161866557+ArDa0226@users.noreply.github.com>
2024-08-11 16:11:41 +0500

    Add files via upload

--- DZ_introspection.py
+++ DZ_introspection.py
@@ -4,13 +4,14 @@ def introspection_info(obj):
     object_type = type(obj)
 
     attribute = dir(obj)
+    attribute_1 = [method for method in attribute if not callable(getattr(obj, method))]
 
     methods = [method for method in attribute if callable(getattr(obj, method))]
 
     module = obj.__class__.__module__
 
     information = {'type': object_type,
-                   'attribute': attribute,
+                   'attribute': attribute_1,
                    'methods': methods,
                    'module': module}
 
@@ -24,4 +25,5 @@ print(str_information)
 
 list_information = introspection_info([2, 34, 4.5, 'Hello'])
 print(list_information)
+
 
